890165ff91a154e921a8f33b68d5b666c04a320c
vadim-cebanu <vadykdeejay@gmail.com>
2026-06-10 19:36:36 +0200

    feat: add login endpoint

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -24,8 +25,21 @@ class RegistrationSerializer(serializers.ModelSerializer):
         
         account = User(
             email=self.validated_data['email'], 
-            username=self.validated_data['fullname']
+            username=self.validated_data['email'],
+            first_name=self.validated_data['fullname']    
         )
         account.set_password(pw)
         account.save()
         return account
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        user= authenticate(username=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials!")
+        data['user']= user
+        return data
--- auth_app/views.py
+++ auth_app/views.py
@@ -2,7 +2,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
-from .api.serializers import RegistrationSerializer
+from .api.serializers import RegistrationSerializer, LoginSerializer
+from rest_framework import status
 
 
 class RegistrationView(APIView):
@@ -11,19 +12,33 @@ class RegistrationView(APIView):
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
         
-        data={}
         if serializer.is_valid():
             saved_account = serializer.save()
             token, created = Token.objects.get_or_create(user=saved_account)
-            data = {
+            return Response({
                 'token': token.key,
-                'fullname': saved_account.username,
+                'fullname': saved_account.first_name,
                 'email': saved_account.email,
                 'user_id': saved_account.id
-            }
-        else:
-            data= serializer.errors
+            }, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
-        return Response(data)
-            
+
+
+class LoginView(APIView):
+    permission_classes= [AllowAny]
+    
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "token": token.key,
+            "fullname": user.first_name,
+            "email": user.email,
+            "user_id": user.id
+            }, status=status.HTTP_200_OK) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                
 
