07c16faed5a61f8059f7b57d13cd6c6ea801e85b
vadim-cebanu <vadykdeejay@gmail.com>
2026-06-10 12:03:33 +0200

    refactor: change file location

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,2 @@
 from rest_framework import serializers
 
-# Serializers moved to respective apps
-# Example: RegistrationSerializer is now in auth_app/serializers.py
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-# URLs moved to respective apps
-# Example: Registration URL is now in auth_app/urls.py
 
 urlpatterns = [
     # Add global API endpoints here if needed
