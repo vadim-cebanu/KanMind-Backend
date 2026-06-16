e7ad1b40322c294b690af36825acddef7b9bc706
vadim-cebanu <vadykdeejay@gmail.com>
2026-06-16 19:20:40 +0200

    refactor: remove old api subdirectories and consolidate structure

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-
-urlpatterns = [
-    # Add global API endpoints here if needed
-]
-
--- boards_app/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from ..views import BoardListView, BoardDetailView
-
-urlpatterns = [
-    path('boards/', BoardListView.as_view(), name='boards'),
-    path('boards/<int:pk>/', BoardDetailView.as_view(), name='board-detail'),  
-]
-
