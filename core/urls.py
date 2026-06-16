729d97d1e12e796b5213637e7ef2ae8328678cbe
vadim-cebanu <vadykdeejay@gmail.com>
2026-06-16 19:20:15 +0200

    fix: url routing for auth and boards endpoints

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth_app.api.urls')),
-    path('api/', include('boards_app.api.urls')),
+    path('api/', include('auth_app.urls')),
+    path('api/', include('boards_app.urls')),
 
 ]
