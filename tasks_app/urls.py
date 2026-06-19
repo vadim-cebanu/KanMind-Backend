35ca0ed9e28025290b1e6d8bdb60295a72b58932
vadim-cebanu <vadykdeejay@gmail.com>
2026-06-19 16:27:25 +0200

    feat: add task and comment url routing

--- tasks_app/urls.py
+++ tasks_app/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('tasks/', views.TaskCreateView.as_view(), name='task-create'),
+    path('tasks/assigned-to-me/', views.TasksAssignedToMeView.as_view(), name='tasks-assigned-to-me'),
+    path('tasks/reviewing/', views.TasksReviewingView.as_view(), name='tasks-reviewing'),
+    path('tasks/<int:task_id>/', views.TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/<int:task_id>/comments/', views.CommentListCreateView.as_view(), name='comment-list-create'),
+    path('tasks/<int:task_id>/comments/<int:comment_id>/', views.CommentDeleteView.as_view(), name='comment-delete'),
+]
 
